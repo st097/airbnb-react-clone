b0ad1749d8b01c1c27ac02c27b1cee1272b5e5d6
st097 <s.tozaj@gmail.com>
2023-09-12 13:06:39 +0200

    all

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src={require("../images/airbnb-logo.png")} className="nav--logo" alt="logo"/>
+        </nav>
+    )
+}
+
